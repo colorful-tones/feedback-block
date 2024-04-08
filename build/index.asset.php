cf8846052bbbc0942ad831d878badc80271bf168
Damon Cook <damonacook@gmail.com>
2024-04-08 16:32:00 -0400

    🚀 launch

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3059a2f05582694406c0');
---
+++ feedback-block.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:       Feedback Block
+ * Description:       Feedback block for single posts
+ * Version:           0.1.0
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       feedback-block
+ *
+ * @package           create-block
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_feedback_block_block_init() {
+	register_block_type_from_metadata( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_feedback_block_block_init' );
