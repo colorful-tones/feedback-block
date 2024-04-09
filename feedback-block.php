3eb8da7217361c1879648449a30d8d4d9d8507f5
Damon Cook <damonacook@gmail.com>
2024-04-09 01:09:25 -0400

    Add WP AJAX actions to handle server form submit
    And send email

--- feedback-block.php
+++ feedback-block.php
@@ -28,3 +28,41 @@ function create_block_feedback_block_block_init() {
 	register_block_type_from_metadata( __DIR__ . '/build' );
 }
 add_action( 'init', 'create_block_feedback_block_block_init' );
+
+/**
+ * Handles the submission of feedback block.
+ *
+ * This function is responsible for processing the feedback submission from the user.
+ * It verifies the nonce, retrieves the necessary data, sends an email to the admin,
+ * and returns a success or fail message.
+ *
+ * @return void
+ */
+function feedback_block_submission_handler() {
+	// Verify nonce
+	if ( ! isset( $_POST['nonce'] ) || ! wp_verify_nonce( $_POST['nonce'], 'feedback_block_nonce' ) ) {
+		die( 'Security check failed' );
+	}
+
+	$post_id        = isset( $_POST['postId'] ) ? intval( $_POST['postId'] ) : 0;
+	$post_title     = get_the_title( $post_id );
+	$post_url       = get_permalink( $post_id );
+	$feedback_text  = sanitize_textarea_field( $_POST['feedback'] );
+	$feedback_emote = sanitize_text_field( $_POST['feedback-form-selected-sentiment'] );
+
+	$to      = get_option( 'admin_email' );
+	$subject = 'Feedback on: ' . $post_title;
+	$message = "Feedback: {$feedback_text}<br> The user's sentiment was: <em>{$feedback_emote}</em> for the post titled <b><a href={$post_url}>{$post_title}</a></b>";
+	$headers = array( 'Content-Type: text/html; charset=UTF-8' );
+
+	if ( wp_mail( $to, $subject, $message, $headers ) ) {
+		echo 'Success';
+	} else {
+		echo 'Fail';
+	}
+
+	wp_die();
+}
+add_action( 'wp_ajax_submit_feedback', 'feedback_block_submission_handler' );
+add_action( 'wp_ajax_nopriv_submit_feedback', 'feedback_block_submission_handler' );
+
